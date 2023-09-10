b6c7e040bb4136a93011cdacac98762794aecb00
Babalwa01 <babalwambolekwa93@gmail.com>
2023-09-10 18:50:11 +0200

    started header

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -1,8 +1,18 @@
 import './Header.scss';
+import { motion } from 'framer-motion';
+import { images } from '../../constants';
 
 const Header = () => {
   return (
-    <header>Header</header>
+    <header id='home' className='app__header app__flex'>
+      <motion.div
+        whileInView={{x: [-100, 0], opacity: [0, 1]}}
+        transition={{ duration: 0.5 }}
+        className='app__header-info'
+      >
+
+      </motion.div>
+    </header>
   )
 }
 
