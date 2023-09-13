25ffd91e3f9a97c183f48679dbbe57d860641110
Babalwa01 <babalwambolekwa93@gmail.com>
2023-09-13 18:38:57 +0200

    fixed about spacing

--- src/container/OurPrograms/OurPrograms.tsx
+++ src/container/OurPrograms/OurPrograms.tsx
@@ -2,7 +2,9 @@ import './OurPrograms.scss';
 
 const OurPrograms = () => {
   return (
-    <section id='Our Programs'>OurPrograms</section>
+    <section id='Our Programs'>
+      <h2>Our <span className='green-text'>Key Activities</span></h2>
+    </section>
   )
 }
 
