84545ac584ce13f1e4aeeabb7be3a3a559e55e1e
Babalwa01 <babalwambolekwa93@gmail.com>
2023-09-13 11:31:25 +0200

    added About us content

--- src/container/AboutUs/AboutUs.tsx
+++ src/container/AboutUs/AboutUs.tsx
@@ -4,29 +4,39 @@ import "./AboutUs.scss";
 const AboutUs = () => {
   return (
     <section id="About Us" className="app__aboutus">
-        <div className="app__aboutus-content_about">
-          <h2 className="headtext__cormorant">About <span className="pink-text">Us</span></h2>
-          <p className="p__opensans">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis
-            pharetra adipiscing ultrices <span className="pink-text bold-text">vulputate posuere tristique.</span> In sed
-            odio nec aliquet eu proin mauris et. <br />Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit. Quis pharetra adipiscing ultrices
-            vulputate <span className="pink-text bold-text">posuere tristique.</span> In sed odio nec aliquet eu proin mauris
-            et.
-          </p>
-        </div>
-        <div className="app__aboutus-content_image">
-          <img src={images.elephant} alt="about_image" />
-        </div>
-        <div className="app__aboutus-content_history">
-          <h2 className="headtext__cormorant"><span className="pink-text">Our</span> History</h2>
-          <p className="p__opensans">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis
-            pharetra adipiscing ultrices <span className="pink-text bold-text">vulputate posuere tristique.</span> In sed
-            odio nec aliquet eu proin mauris et. Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit.
-          </p>
-        </div>
+      <div className="app__aboutus-content_about">
+        <h2 className="headtext__cormorant">
+          About <span className="pink-text">Us</span>
+        </h2>
+        <p className="p__opensans">
+          Bayathandwa Day Care Centre is a registered Non-Profit Organization
+          implementing{" "}
+          <span className="pink-text bold-text">
+            Early Childhood Development
+          </span>{" "}
+          from 4 months to 5 years. It is located at Dibinkonzo Village in
+          Sterkspruit, Eastern Cape. <br />
+          We operate from Monday to Friday from 06:00 to 18:00 and provide{" "}
+          <span className="pink-text bold-text">after-care services</span> when
+          needed.
+        </p>
+      </div>
+      <div className="app__aboutus-content_image">
+        <img src={images.elephant} alt="about_image" />
+      </div>
+      <div className="app__aboutus-content_history">
+        <h2 className="headtext__cormorant">
+          <span className="pink-text">Our</span> History
+        </h2>
+        <p className="p__opensans">
+          Bayathandwa Day Care Centre was founded by Lucia Mbolekwa in 2015 and
+          continues to make a{" "}
+          <span className="pink-text bold-text">positive difference</span> in
+          the lives of our children through the support of the Sterkspruit
+          community, the Department of Education, the Department of Social
+          Development and National Lotteries Commission.
+        </p>
+      </div>
     </section>
   );
 };
