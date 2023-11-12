da407fa83e0287f77757a1d8ff9ce1e90837cb7e
Babalwa01 <babalwambolekwa93@gmail.com>
2023-11-12 23:07:37 +0200

    compressed activities images

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,7 +1,6 @@
 import logo from "../assets/logo.png";
 import about01 from "../assets/about01.jpg";
 import about02 from "../assets/about02.jpg";
-import elephant from "../assets/elephant.jpg";
 import header1 from "../assets/header1.jpg";
 import header2 from "../assets/header2.jpg";
 import clinic from "../assets/clinic.jpg";
@@ -24,7 +23,6 @@ export default {
   logo,
   about01,
   about02,
-  elephant,
   header1,
   header2,
   clinic,
