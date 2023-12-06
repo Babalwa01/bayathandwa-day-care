0d6f8b953817ade82b9f577edbb490f7bace8f11
Babalwa01 <babalwambolekwa93@gmail.com>
2023-12-06 21:22:52 +0200

    added passive listeners

--- main.ts
+++ main.ts
@@ -1,10 +1,14 @@
 // Make sure sw are supported
 if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("./sw_cached_pages.js")
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      .then((reg) => console.log("Service worker: Registered"))
-      .catch((err) => console.log(`Service Worker: Error: ${err}`));
-  });
+  window.addEventListener(
+    "load",
+    () => {
+      navigator.serviceWorker
+        .register("./sw_cached_pages.js")
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        .then((reg) => console.log("Service worker: Registered"))
+        .catch((err) => console.log(`Service Worker: Error: ${err}`));
+    },
+    { passive: true }
+  );
 }
--- sw_cached_pages.js
+++ sw_cached_pages.js
@@ -2,44 +2,56 @@ const cacheName = "v1";
 
 // Call install event
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-self.addEventListener("install", (e) => {
-  console.log("Service Worker: Installed");
-});
+self.addEventListener(
+  "install",
+  (e) => {
+    console.log("Service Worker: Installed");
+  },
+  { passive: true }
+);
 
 // Call activate event
-self.addEventListener("activate", (e) => {
-  console.log("Service Worker: Activated");
-  e.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== cacheName) {
-            console.log("Service Worker: Clearing Old Cache");
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
-});
+self.addEventListener(
+  "activate",
+  (e) => {
+    console.log("Service Worker: Activated");
+    e.waitUntil(
+      caches.keys().then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cache) => {
+            if (cache !== cacheName) {
+              console.log("Service Worker: Clearing Old Cache");
+              return caches.delete(cache);
+            }
+          })
+        );
+      })
+    );
+  },
+  { passive: true }
+);
 
 // Call fetch event
-self.addEventListener("fetch", (e) => {
-  console.log("Service Worker: Fetching");
-  e.respondWith(
-    fetch(e.request)
-      .then((res) => {
-        // Make a copy / clone of the response
-        const resClone = res.clone();
-        // Open cache
-        caches.open(cacheName).then((cache) => {
-          // Add response to cache
-          cache.put(e.request, resClone);
-        });
-        return res;
+self.addEventListener(
+  "fetch",
+  (e) => {
+    console.log("Service Worker: Fetching");
+    e.respondWith(
+      fetch(e.request)
+        .then((res) => {
+          // Make a copy / clone of the response
+          const resClone = res.clone();
+          // Open cache
+          caches.open(cacheName).then((cache) => {
+            // Add response to cache
+            cache.put(e.request, resClone);
+          });
+          return res;
+          // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        })
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      })
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      .catch((err) => caches.match(e.request).then((res) => res))
-  );
-});
+        .catch((err) => caches.match(e.request).then((res) => res))
+    );
+  },
+  { passive: true }
+);
